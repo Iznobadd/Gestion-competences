8a72ab6f74c0931ddeee32a482c24319d8b7c05a
Brandon CEBI <brandon.cebi.pro@gmail.com>
2022-06-04 14:15:52 +0200

    add infos and experiences in profile template

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\ExperiencesRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,11 +11,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    public function index(UserRepository $userRepository, ExperiencesRepository $experiencesRepository): Response
     {
         if($this->getUser())
         {
-            return $this->render('dashboard/profile.html.twig');
+            $identifier = $this->getUser()->getUserIdentifier();
+            $user = $userRepository->findOneBy(['email' => $identifier]);
+            $exps = $experiencesRepository->findBy(['user' => $user]);
+            return $this->render('dashboard/profile.html.twig', compact('user', 'exps'));
         }
         return $this->redirectToRoute('app_login');
     }
