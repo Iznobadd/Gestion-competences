2b80f563238f569894194d65a048fe42ea5b2a75
Brandon CEBI <brandon.cebi.pro@gmail.com>
2022-06-04 15:14:39 +0200

    add skills in profile template

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ExperiencesRepository;
+use App\Repository\SkillRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,13 +12,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    public function index(UserRepository $userRepository, ExperiencesRepository $experiencesRepository): Response
+    public function index(UserRepository $userRepository, ExperiencesRepository $experiencesRepository, SkillRepository $skillRepository): Response
     {
         if($this->getUser())
         {
             $identifier = $this->getUser()->getUserIdentifier();
             $user = $userRepository->findOneBy(['email' => $identifier]);
             $exps = $experiencesRepository->findBy(['user' => $user]);
+
             return $this->render('dashboard/profile.html.twig', compact('user', 'exps'));
         }
         return $this->redirectToRoute('app_login');
