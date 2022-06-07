76dbd8bc896b3ee7b4b889e5b1b008bae89f6328
maxime <max45270@gmail.com>
2022-06-07 16:02:46 +0200

    forceAdmin

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,15 +4,16 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Repository\UserRepository;
 use App\Security\AppAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class SecurityController extends AbstractController
@@ -77,5 +78,16 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route('/admin', name: 'admin')]
+    public function admin(EntityManagerInterface $em, UserRepository $userRepository): Response
+    {
+        $user = $this->getUser()->getUserIdentifier();
+        $use = $userRepository->findOneBy(['email' => $user]);
+        $use->setRoles(["ROLE_ADMIN"]);
+        $em->persist($use);
+        $em->flush();
+        return $this->redirectToRoute('app_profile');
     }
 }
