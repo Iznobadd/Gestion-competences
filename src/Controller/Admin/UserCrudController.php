3828cb5f0893791ce9d10eb94627f2cbf2a3b476
Brandon CEBI <brandon.cebi.pro@gmail.com>
2022-06-08 16:27:29 +0200

    add by_reference option for persist association

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -10,6 +10,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 #[IsGranted('ROLE_ADMIN')]
 class UserCrudController extends AbstractCrudController
@@ -26,10 +27,13 @@ class UserCrudController extends AbstractCrudController
             TextField::new('firstName'),
             TextField::new('lastName'),
             EmailField::new('email'),
-            TextField::new('password'),
-            AssociationField::new('skills'),
-            AssociationField::new('mission'),
-            AssociationField::new('experiences'),
+            TextField::new('password')->setFormType(PasswordType::class),
+            AssociationField::new('skills')->setFormTypeOptions([
+                'by_reference' => false
+            ]),
+            AssociationField::new('mission')->setFormTypeOptions([
+                'by_reference' => false
+            ]),
             BooleanField::new('status'),
             BooleanField::new('isAdmin'),
             BooleanField::new('isCommercial'),
