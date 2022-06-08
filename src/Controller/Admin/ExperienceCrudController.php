c121d4e4e06f8305e4e84d77d6f2680eb46ae5de
Brandon CEBI <brandon.cebi.pro@gmail.com>
2022-06-08 11:10:31 +0200

    add admin role for admin dashboard

--- src/Controller/Admin/ExperienceCrudController.php
+++ src/Controller/Admin/ExperienceCrudController.php
@@ -4,7 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Experience;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+#[IsGranted('ROLE_ADMIN')]
 class ExperienceCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
--- src/Controller/Admin/SkillCrudController.php
+++ src/Controller/Admin/SkillCrudController.php
@@ -4,7 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Skill;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+#[IsGranted('ROLE_ADMIN')]
 class SkillCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
